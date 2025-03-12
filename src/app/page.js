bb0d2c16181e51402fd3cd6b8c9b106f3e40aece
GastonCarg <gastoncargnelutti@gmail.com>
2025-03-12 15:22:38 -0300

    feat: add painting view

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,101 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const ROWS = 100;
+const COLORS = ["red", "yellow", "blue", "purple", "orange"];
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+export default function Home() {
+  const [coloredCells, setColoredCells] = useState({});
+  const [contextMenu, setContextMenu] = useState({ visible: false, x: 0, y: 0, cell: null });
+  const [principalColor, setPrincipalColor] = useState('red');
+  const [clickedMouseDown, setClickedMouseDown] = useState(false);
+  const [listCellToColor, setListCellToColor] = useState(new Set());
+
+  const screenHeight = typeof window !== undefined ? window.innerHeight : 0;
+
+  const handleClick = (cell, color) => {
+    setPrincipalColor(color);
+    setColoredCells((prevState) => ({
+      ...prevState,
+      [cell]: prevState[cell] === color ? 'white' : color,
+    }));
+  };
+
+  const handleRightClick = (event, cell) => {
+    event.preventDefault();
+    setContextMenu({ visible: true, x: event.clientX, y: event.clientY, cell });
+  };
+
+  const selectColor = (color) => {
+    if (contextMenu.cell !== null) {
+      handleClick(contextMenu.cell, color);
+      setContextMenu({ visible: false, x: 0, y: 0, cell: null });
+    }
+  };
+
+  const handleMouseDown = (idx) => {
+    handleClick(idx, principalColor);
+    setClickedMouseDown(true);
+  };
+
+  const handleMouseUp = (idx) => {
+    handleClick(idx, principalColor);
+    setClickedMouseDown(false);
+
+    listCellToColor.map((cell) => {
+      handleClick(cell, principalColor);
+    });
+
+    setListCellToColor([]);
+  };
+
+  const handleMouseOver = (idx) => {
+    if (!clickedMouseDown) return;
+
+    setListCellToColor(prevState => {
+      const newSet = new Set(prevState);
+      if (newSet.has(idx)) return prevState;
+
+      newSet.add(idx);
+      return Array.from(newSet);
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      {Array.from({ length: ROWS * screenHeight / 13 }, (_, idx) => (
+        <section
+          key={idx}
+          className={styles.cell}
+          onClick={() => handleClick(idx, principalColor)}
+          onContextMenu={(event) => handleRightClick(event, idx)}
+          style={{ backgroundColor: coloredCells[idx] || "white" }}
+          onMouseDown={() => handleMouseDown(idx)}
+          onMouseOver={() => handleMouseOver(idx)}
+          onMouseUp={() => handleMouseUp(idx)}
+        ></section>
+      ))}
+
+      {contextMenu.visible && (
+        <div
+          className={styles.colorpicker}
+          style={{ top: contextMenu.y, left: contextMenu.x }}
+        >
+          {
+            COLORS.map((color) => (
+              <div
+                key={color}
+                className={styles.coloroption}
+                style={{ backgroundColor: color }}
+                onClick={() => selectColor(color)}
+              ></div>
+            ))
+          }
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
     </div>
   );
 }
